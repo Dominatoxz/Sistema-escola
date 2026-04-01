9280103a3a3f4626d05b9b5edf1fad7d5371d689
Dominatoxz <dominatomathus06@gmail.com>
2026-04-01 16:54:21 -0300

    updated 01/04

--- Controller/EstudanteController.php
+++ Controller/EstudanteController.php
@@ -12,6 +12,26 @@ class EstudanteController {
         //instanciar a Model Estudante
         $this->estudante = new Estudante($this->db);
     }
+
+    /*Ação*/
+    //listar todos os alunos na tela inicial
+    public function index() {
+        //pede lista de dados ao Model
+        $alunos = $this->estudante->buscarTodos();
+
+        require_once 'index.php';
+    } 
+
+    public function salvar() {
+        //verifica se o fomulário foi enviado via POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $dados = [
+                'nome' => htmlspecialchars(trim($_POST['nome']), ENT_QUOTES, 'UTF-8'),
+                'email' => htmlspecialchars(trim($_POST['email']), ENT_QUOTES, 'UTF-8'),
+                'matricula' => htmlspecialchars(trim($_POST['matricula']), ENT_QUOTES, 'UTF-8')
+            ];
+        }
+    }
   
 }
 
